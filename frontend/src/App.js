3fd3102bb20a28b1508db163c9a8fb914a5663bf
sandro paval <derray99@gmail.com>
2022-02-20 22:20:59 +0100

    First commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
     super(props);
     this.state= {
       animes: [],
+      favoriten: [],
     }
   }
 
@@ -18,20 +19,43 @@ class App extends Component {
       const animes = res.data;
       this.setState({animes});
     })
-    
+
+    axios.get('http://localhost:8080/Anime/Favorite')
+        .then(res => {
+            const favoriten = res.data;
+            this.setState({favoriten});
+        })
   }
+
 
 
 render() {
 
           return(
+              <div className="homePage"> <h1 className="homePageHeader">Der Mac Gillis Fanclub</h1>
+                <div className="favoritenListe">
+                    <h1 className="favoritenHeader">Favoriten: </h1>
+                    <ul>
+                        {this.state.favoriten.map(favorit => (
+                            <p  key={favorit.animeID} className="animeCertain">
+                                <h1 className="animeTitel"> Titel: {favorit.titel}</h1>
+                                <img className="animeBild" src={favorit.img} alt='' />
+                                <ul className="animeBeschreibung">
+                                    <h2> Genre: {favorit.genre}</h2>
+                                    <h2> Folgen: {favorit.folgen}</h2>
+                                    <h2> Wertung: {favorit.wertung}</h2>
+                                </ul>
+                            </p>
+                        ))}+
+                    </ul>
 
 
+                </div>
               <div className="animeListe">
-
+                    <h1 className="animeHeader">Animes: </h1>
                   <ul>
                       {this.state.animes.map(anime => (
-                          <p className="animeCertain">
+                          <p key={anime.animeID} className="animeCertain">
                               <h1 className="animeTitel"> Titel: {anime.titel}</h1>
                               <img className="animeBild" src={anime.img} alt='' />
                               <ul className="animeBeschreibung">
@@ -40,12 +64,12 @@ render() {
                               <h2> Wertung: {anime.wertung}</h2>
                               </ul>
                           </p>
-                          ))};
+                          ))}
                       </ul>
 
 
                   </div>
-
+              </div>
 
           );
 }
