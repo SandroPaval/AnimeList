757574acc5ee37c4eb20deab8c83e131216d5645
sandro paval <derray99@gmail.com>
2022-02-21 18:27:37 +0100

    Added FavoriteList, CompleteList, Top List

---
+++ frontend/src/components/AnimeListe.js
@@ -0,0 +1,25 @@
+import React from "react";
+import './AnimeListe.css'
+
+export default function AnimeListe (props){
+    const {animeID, titel, img, genre, folgen, wertung} = props.anime;
+    return (
+    <div className="homePage">
+        <div>
+
+        <ul>
+                <p key={animeID} className="animeCertain">
+                    <h1 className="animeTitel"> Titel: {titel}</h1>
+                    <img className="animeBild" src={img} alt='' />
+                    <ul className="animeBeschreibung">
+                        <h2> Genre: {genre}</h2>
+                        <h2> Folgen: {folgen}</h2>
+                        <h2> Wertung: {wertung}</h2>
+                    </ul>
+                </p>
+        </ul>
+    </div>
+</div>
+
+        );
+}
