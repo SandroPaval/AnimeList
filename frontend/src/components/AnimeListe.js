cac9faaa8d71a704a4f9cabe7bdfdf5f399c0a18
sandro paval <derray99@gmail.com>
2022-03-04 21:06:03 +0100

    tested favoriteButton

--- frontend/src/components/AnimeListe.js
+++ frontend/src/components/AnimeListe.js
@@ -1,21 +1,26 @@
+
 import React from "react";
 import './AnimeListe.css'
 
+
 export default function AnimeListe (props){
     const {animeID, titel, img, genre, folgen, wertung} = props.anime;
+    
+  
+
     return (
         <div>
 
         <ul>
-                <p key={animeID} className="animeCertain">
-                    <h1 className="animeTitel"> {titel}</h1>
+                <div key={animeID} className="animeCertain">
+                    <h1 className="animeTitel"> {titel} </h1>
                     <img className="animeBild" src={img} alt='' />
                     <ul className="animeBeschreibung">
                         <h2 className="descFont"> Genre: {genre}</h2>
                         <h2 className="descFont"> Folgen: {folgen}</h2>
                         <h2 className="descFont"> Wertung: {wertung}</h2>
                     </ul>
-                </p>
+                </div>
         </ul>
     </div>
 
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,7 +3,7 @@ export default function () {
     return (
         <div className="HomePage">
             <img className="macGillis" src="https://qph.fs.quoracdn.net/main-qimg-1d5035b9233bdb9e1b124f757c616fd2-lq" />
-            <p className="text">Herzlich willkommen beim MacGillis Fanclub! wir grüßen jeden mysteriösen Maskenmann Sympatisianten</p>
+            <p className="text">Herzlich willkommen beim MacGillis Fanclub! wir grüßen jeden mysteriösen Maskenmann Sympathisanten</p>
         </div>
 
     )
